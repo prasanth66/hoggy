c7599a62232f1c21feadb69cbfac0fdf84778d7d
kundella prasanth yadav <prasanthkdp6@gmail.com>
2022-01-18 09:02:36 +0530

    set home page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hoggy/views/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  Container(),
+      home:  HomePage(),
     );
   }
 }
